54fd38c0ed76216f48f13269d961f18e576d3fee
saarthak-agarwal <sarthak1607@gmail.com>
2020-09-12 11:57:34 +0530

    Sorry for the terrible audio. Smallest I could find.

--- lib/sounds.py
+++ lib/sounds.py
@@ -1,9 +1,10 @@
 from pydub.playback import play
 from math import ceil
+import pydub
 
 def createSoundEffect(sound_file, video_length):
 	sound = pydub.AudioSegment.from_wav(sound_file)
-	length_sound_file = length(sound)
+	length_sound_file = len(sound)
 	if length_sound_file >= video_length:
 		return
 	loops_required = ceil(video_length/length_sound_file)
@@ -12,3 +13,16 @@ def createSoundEffect(sound_file, video_length):
 
 def playSound(sound_object):
 	play(sound_object)
+
+# FOR TESTING: 
+'''
+sound = pydub.AudioSegment.from_wav("audio.wav")
+length_sound_file = len(sound)
+print("LENGTH: ",length_sound_file)
+print("Playing once: ")
+play(sound)
+sound1 = sound*2
+print("LENGTH: ", len(sound1))
+print("Playing twice: ")
+play(sound1)
+'''
