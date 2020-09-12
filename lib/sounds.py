5038c8f7db8d7b8f9952ebcc8a3a8c5979fe8ce9
saarthak-agarwal <sarthak1607@gmail.com>
2020-09-12 12:34:20 +0530

    UPD: Write audio to new file

--- lib/sounds.py
+++ lib/sounds.py
@@ -2,17 +2,14 @@ from pydub.playback import play
 from math import ceil
 import pydub
 
-def createSoundEffect(sound_file, video_length):
+def createSoundEffect(sound_file, video_length = 20):
 	sound = pydub.AudioSegment.from_wav(sound_file)
-	length_sound_file = len(sound)
+	length_sound_file = len(sound)/1000
 	if length_sound_file >= video_length:
 		return
 	loops_required = ceil(video_length/length_sound_file)
 	new_sound = sound*loops_required
-	return new_sound 
-
-def playSound(sound_object):
-	play(sound_object)
+	new_sound.export("new_audio.wav", format="wav") 
 
 # FOR TESTING: 
 '''
@@ -25,4 +22,6 @@ sound1 = sound*2
 print("LENGTH: ", len(sound1))
 print("Playing twice: ")
 play(sound1)
+sound1.export("new_audio.wav", format="wav")
 '''
+# createSoundEffect("audio.wav")
