11fc51808df8ec6d94ba1ea4deeac4b48bbaa49c
Anirudh H M <anihm136@gmail.com>
2020-09-12 19:33:18 +0530

    Start work on transitions

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import ffmpeg
 from sounds import createSoundEffect
 from utils import getImagesFromPath, orderImages
+from transitions import applyTransitions
+from transitionsLib.crossFade import crossFade
 
 
 def createSlideshow(images_path, frame_duration, transition_duration):
@@ -19,10 +21,10 @@ def createSlideshow(images_path, frame_duration, transition_duration):
     frame_images = getImagesFromPath(images_path)
     sorted_images = orderImages(frame_images)
     vid_length = len(sorted_images) * frame_duration
-    # im_streams = applyTransitions(sorted_images)
+    im_stream = applyTransitions(sorted_images, crossFade, frame_duration, transition_duration)
     createSoundEffect("audio.wav", vid_length)
     audio_strem = ffmpeg.input("new_audio.wav")
-    out = ffmpeg.output(concat_images, audio_strem, "sorted_images.mp4", t=vid_length, r=FRAMERATE).overwrite_output()
+    out = ffmpeg.output(im_stream, audio_strem, "sorted_images.mp4", t=vid_length, r=FRAMERATE).overwrite_output()
     # out = ffmpeg.output(concat_images, audio_strem, "with_audio.avi").overwrite_output()
     pp(ffmpeg.compile(out))
     # print(" ".join(ffmpeg.compile(out)))
--- lib/transitions.py
+++ lib/transitions.py
@@ -7,18 +7,21 @@ def applyTransitions(
     im_streams = []
     for file in image_paths:
         im_streams.append(ffmpeg.input(file, t=frame_duration, loop=1))
-    for i in range(len(im_streams)):
-        im_streams[i] = (
-            im_streams[i]
-            .filter(
-                "scale",
-                width=1280,
-                height=720,
-                force_original_aspect_ratio="decrease",
-            )
-            .filter("pad", width=1280, height=720, x="(ow-iw)/2", y="(oh-ih)/2")
-            .filter("setsar", 1)
-        )
-    im_streams = transition_function(im_streams)
-    concat_images = ffmpeg.concat(*im_streams, v=1, a=0)
+    filterset = []
+    # for i in range(len(im_streams)):
+    #     im_streams[i] = (
+    #         im_streams[i]
+    #         .filter(
+    #             "scale",
+    #             width=1280,
+    #             height=720,
+    #             force_original_aspect_ratio="decrease",
+    #         )
+    #         .filter("pad", width=1280, height=720, x="(ow-iw)/2", y="(oh-ih)/2")
+    #         .filter("setsar", 1)
+    #     )
+    for i in range(len(im_streams)-1):
+        filterset.append(ffmpeg.filter([im_streams[i], im_streams[i+1]], 'xfade', transition='fade', duration=1, offset=4.5))
+    # im_streams = transition_function(im_streams)
+    concat_images = ffmpeg.concat(*filterset, v=1, a=0)
     return concat_images
