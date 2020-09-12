e40fea03b5a47d07729e462b55ef6deb8f43e3ae
Anirudh H M <anihm136@gmail.com>
2020-09-12 20:40:38 +0530

    Handle defaults and missing values

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,36 +1,44 @@
-from glob import glob  # Testing only
 from pathlib import Path
-from pprint import pp  # Testing only
 
 import ffmpeg
-from sounds import createSoundEffect
-from transitions import applyTransitions
-from utils import getImagesFromPath, orderImages
+
+from .sounds import createSoundEffect
+from .transitions import applyTransitions
+from .utils import getImagesFromPath, orderImages
 
 
-def createSlideshow(images_path, frame_duration, transition_duration):
+def createSlideshow(
+    images_path,
+    music_path,
+    frame_duration,
+    frame_rate,
+    image_resolution,
+    transition_duration,
+):
     images_path = Path(images_path)
     assert images_path.is_dir(), "Given path is not a directory"
     assert isinstance(frame_duration, int), "Frame duration must be an integer"
     assert isinstance(
         transition_duration, int
     ), "Transition duration must be an integer"
-    FRAMERATE = 5
+
     frame_images = getImagesFromPath(images_path)
     sorted_images = orderImages(frame_images)
     vid_length = len(sorted_images) * frame_duration
-    im_stream = applyTransitions(
-        sorted_images, (1920, 1080), "wipeup", frame_duration, transition_duration
+    output_streams = []
+    output_streams.append(
+        applyTransitions(
+            sorted_images,
+            image_resolution,
+            "wipeup",
+            frame_duration,
+            transition_duration,
+        )
     )
-    createSoundEffect("audio.wav", vid_length)
-    audio_strem = ffmpeg.input("new_audio.wav")
+    if music_path:
+        createSoundEffect(music_path, vid_length)
+        output_streams.append(ffmpeg.input("new_audio.wav"))
     out = ffmpeg.output(
-        im_stream, audio_strem, "sorted_images.mp4", t=vid_length, r=FRAMERATE
+        *output_streams, "sorted_images.mp4", t=vid_length, r=frame_rate
     ).overwrite_output()
-    # out = ffmpeg.output(concat_images, audio_strem, "with_audio.avi").overwrite_output()
-    pp(ffmpeg.compile(out))
-    # print(" ".join(ffmpeg.compile(out)))
     ffmpeg.run(out)
-
-
-createSlideshow("../img", 5, 1)
