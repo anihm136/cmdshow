ba491a1e014c7ad3b04e0af22dad899c6cdf523f
Anirudh H M <anihm136@gmail.com>
2020-09-13 14:20:28 +0530

    Remove unused utils functions

--- lib/utils.py
+++ lib/utils.py
@@ -57,28 +57,6 @@ def orderImages(images, parameter="sim"):
     return sorted_images
 
 
-def createDirectory(parent, child):
-    """
-    Create a directory if it does not exist, and clear the directory if it does
-
-    :param parent [TODO:type]: [TODO:description]
-    :param child [TODO:type]: [TODO:description]
-    """
-    directoryPath = Path(parent)
-    tempPath = directoryPath / f"{child}"
-    try:
-        tempPath.mkdir(parents=True)
-    except FileExistsError:
-        for f in tempPath.iterdir():
-            f.unlink()
-    return tempPath
-
-
-def copyImagesToDirectory(images, ToPath):
-    for img in images:
-        shutil.copy(img, ToPath)
-
-
 class Spinner:
     spinner_cycle = itertools.cycle(["⢄", "⢂", "⢁", "⡁", "⡈", "⡐", "⡠"])
 
@@ -101,79 +79,3 @@ class Spinner:
             sys.stdout.flush()
             time.sleep(0.05)
             sys.stdout.write("\b")
-
-
-"""
-def resizeToMean(images):
-    num_images = len(images)
-    mean_width, mean_height = 0, 0
-
-    for f in images:
-        img = Image.open(f)
-        width, height = img.size
-        mean_width+= width
-        mean_height+= height
-    
-    mean_width= int(mean_width/num_images)
-    mean_height= int(mean_height/num_images)
-
-    for f in images:
-        img = Image.open(f)
-        imResize = img.resize((mean_width, mean_height), Image.ANTIALIAS)  
-        imResize.save(f, 'JPEG', quality = 100) 
-
-def resizeToMax(images):
-    max_width = float("-inf")
-    max_height = float("-inf")
-    for f in images:
-        img = Image.open(f)
-        width, height = img.size
-        if width > max_width and height > max_height:
-            max_width = width
-            max_height = height
-
-    for f in images:
-        img = Image.open(f)
-        imResize = img.resize((max_width, max_height), Image.ANTIALIAS)  
-        imResize.save(f, 'JPEG', quality = 100) 
-
-def resizeToMin(images):
-    min_width = float("inf")
-    min_height = float("inf")
-    for f in images:
-        img = Image.open(f)
-        width, height = img.size
-        if width < min_width and height < min_height:
-            min_width = width
-            min_height = height
-
-    for f in images:
-        img = Image.open(f)
-        imResize = img.resize((min_width, min_height), Image.ANTIALIAS)  
-        imResize.save(f, 'JPEG', quality = 100) 
-
-
-def resizeImages(imgPath, mode='mean'):
-    images = getImagesFromPath(imgPath)
-
-    tmpPath = createDirectory(imgPath, "tmp-mean")
-    copyImagesToDirectory(images, tmpPath)
-
-    tmpImages = getImagesFromPath(tmpPath)
-    resizeToMean(tmpImages)
-
-    tmpPath = createDirectory(imgPath, "tmp-max")
-    copyImagesToDirectory(images, tmpPath)
-
-    tmpImages = getImagesFromPath(tmpPath)
-    resizeToMax(tmpImages)
-
-    tmpPath = createDirectory(imgPath, "tmp-min")
-    copyImagesToDirectory(images, tmpPath)
-
-    tmpImages = getImagesFromPath(tmpPath)
-    resizeToMin(tmpImages)
-
-"""
-# p = r"../img"
-# print(orderImages(getImagesFromPath(p)))
