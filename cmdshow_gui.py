84ce9b385d7955a4e7455314930a3e2dc3dbf44e
aditeyabaral <aditeya.baral@gmail.com>
2020-09-13 16:44:12 +0530

    Added new GUI

--- cmdshow_gui.py
+++ cmdshow_gui.py
@@ -14,11 +14,10 @@ class App:
         self.root = Tk()
         self.root.title("CMDShow")
         self.root.configure(background="#d2d2c9")
-        # self.root.geometry("800x500")
 
-        self.imageDir = ""
-        self.outputDir = "slideshow.mp4"
-        self.audio = ""
+        self.imageDir = StringVar()
+        self.outputDir = StringVar()
+        self.audio = StringVar()
         self.resolution = ""
         self.frameDuration = ""
         self.transitionType = ""
@@ -26,15 +25,18 @@ class App:
         self.transitionDuration = ""
         self.createdSlideshow = ""
         self.error = StringVar()
+        self.imageDir.set("No image directory chosen")
+        self.outputDir.set("slideshow.mp4")
+        self.audio.set("No output directory chosen")
 
         self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.grid(row=0, columnspan=6)
 
         self.welcome = Label(
-            self.root, text="Welcome to CMDShow!", background="#d2d2c9"
+            self.root, text="CMDShow!", background="#d2d2c9"
         )
-        self.welcome.config(fg="#6d031c", font=("Comfortaa", 40))
-        self.welcome.grid(row=1, columnspan=6, sticky="NSEW")
+        self.welcome.config(fg="#6d031c", font=("Roboto", 40))
+        self.welcome.grid(row=1, columnspan=6, sticky="NSEW", padx = 200)
 
         self.blank = Label(self.root, bg="#d2d2c9")
         self.blank.grid(row=2, columnspan=6)
@@ -47,7 +49,19 @@ class App:
             fg="white",
             font=("Comfortaa", 15),
         )
-        self.imagesButton.grid(row=3, column=0, columnspan=2)
+        self.imagesButton.grid(row=3, column=0, columnspan=3)
+
+        self.imgdir_content = Message(
+            self.root,
+            textvariable=self.imageDir,
+            bg="#d2d2c9",
+            font=("Calibri"),
+            fg="#673e37",
+        )
+        self.imgdir_content.grid(row=3, column = 3, columnspan=3, sticky = "WE")
+
+        self.blank = Label(self.root, bg="#d2d2c9")
+        self.blank.grid(row=4, columnspan=6)
 
         self.audioButton = Button(
             self.root,
@@ -57,7 +71,19 @@ class App:
             fg="white",
             font=("Comfortaa", 15),
         )
-        self.audioButton.grid(row=3, column=2, columnspan=2)
+        self.audioButton.grid(row=5, column=0, columnspan=3)
+
+        self.audiodir_content = Message(
+            self.root,
+            textvariable=self.audio,
+            bg="#d2d2c9",
+            font=("Calibri"),
+            fg="#673e37",
+        )
+        self.audiodir_content.grid(row=5, column = 3, columnspan=3)
+
+        self.blank = Label(self.root, bg="#d2d2c9")
+        self.blank.grid(row=6, columnspan=6)
 
         self.output = Button(
             self.root,
@@ -67,35 +93,53 @@ class App:
             fg="white",
             font=("Comfortaa", 15),
         )
-        self.output.grid(row=3, column=4, columnspan=2)
+        self.output.grid(row=7, column=0, columnspan=3)
+
+        self.opdir_content = Message(
+            self.root,
+            textvariable=self.outputDir,
+            bg="#d2d2c9",
+            font=("Calibri"),
+            fg="#673e37"
+        )
+        self.opdir_content.grid(row=7, column = 3, columnspan=3)
 
         self.blank = Label(self.root, bg="#d2d2c9")
-        self.blank.grid(row=4, columnspan=6)
+        self.blank.grid(row=8, columnspan=6)
+
+        self.config = Label(
+            self.root, text="SETTINGS", background="#d2d2c9"
+        )
+        self.config.config(fg="#6d031c", font=("Comfortaa", 15))
+        self.config.grid(row=9, column = 2, columnspan=2, sticky="NSEW")
+
+        self.blank = Label(self.root, bg="#d2d2c9")
+        self.blank.grid(row=10, columnspan=6)
 
         self.transition_dur = Label(
             self.root, text="Transition Duration", background="#d2d2c9"
         )
         self.transition_dur.config(fg="#673e37", font=("Comfortaa", 15))
-        self.transition_dur.grid(row=5, column=0, padx=5)
+        self.transition_dur.grid(row=11, column=0, columnspan = 3, padx=5)
 
         self.transition_dur_entry = Entry(self.root)
-        self.transition_dur_entry.grid(row=5, column=1)
+        self.transition_dur_entry.grid(row=11, column=3, columnspan = 3)
         self.transition_dur_entry.insert(0, "2")
 
         self.frame_dur = Label(
             self.root, text="Frame Duration", background="#d2d2c9")
         self.frame_dur.config(fg="#673e37", font=("Comfortaa", 15))
-        self.frame_dur.grid(row=5, column=2, padx=5)
+        self.frame_dur.grid(row=12, column=0, columnspan = 3, padx=5)
 
         self.frame_dur_entry = Entry(self.root)
-        self.frame_dur_entry.grid(row=5, column=3)
+        self.frame_dur_entry.grid(row=12, column=3, columnspan = 3)
         self.frame_dur_entry.insert(0, "5")
 
         self.transition_type = Label(
             self.root, text="Transition Type", background="#d2d2c9"
         )
         self.transition_type.config(fg="#673e37", font=("Comfortaa", 15))
-        self.transition_type.grid(row=5, column=4, padx=5)
+        self.transition_type.grid(row=13, column=0, columnspan = 3, padx=5)
 
         self.transition_type_entry = ttk.Combobox(
             self.root, textvariable=self.transitionType
@@ -145,31 +189,29 @@ class App:
             "fadegrays",
         ]
         self.transition_type_entry.current(1)
-        self.transition_type_entry.grid(row=5, column=5, padx=10)
+        self.transition_type_entry.grid(row=13, column=3, columnspan=3, padx=10)
 
-        self.blank = Label(self.root, bg="#d2d2c9")
-        self.blank.grid(row=6, columnspan=6)
 
         self.frame_rate = Label(
             self.root, text="Frame Rate", background="#d2d2c9")
         self.frame_rate.config(fg="#673e37", font=("Comfortaa", 15))
-        self.frame_rate.grid(row=7, column=0, columnspan=1)
+        self.frame_rate.grid(row=14, column=0, columnspan=3)
 
         self.frame_rate_entry = Entry(self.root)
-        self.frame_rate_entry.grid(row=7, column=1, columnspan=2)
+        self.frame_rate_entry.grid(row=14, column=3, columnspan=3)
         self.frame_rate_entry.insert(0, "15")
 
         self.show_resolution = Label(
             self.root, text="Resolution", background="#d2d2c9")
         self.show_resolution.config(fg="#673e37", font=("Comfortaa", 15))
-        self.show_resolution.grid(row=7, column=3, columnspan=1)
+        self.show_resolution.grid(row=15, column=0, columnspan=3)
 
         self.resolution_entry = Entry(self.root)
-        self.resolution_entry.grid(row=7, column=4, columnspan=2)
+        self.resolution_entry.grid(row=15, column=3, columnspan=3)
         self.resolution_entry.insert(0, "1920x1080")
 
         self.blank = Label(self.root, bg="#d2d2c9")
-        self.blank.grid(row=8, columnspan=6)
+        self.blank.grid(row=16, columnspan=6)
 
         self.content = Message(
             self.root,
@@ -178,7 +220,7 @@ class App:
             font=("Calibri"),
             fg="#673e37",
         )
-        self.content.grid(row=9, columnspan=6)
+        self.content.grid(row=17, columnspan=6)
 
         self.generateButton = Button(
             self.root,
@@ -188,7 +230,7 @@ class App:
             fg="white",
             font=("Comfortaa", 15),
         )
-        self.generateButton.grid(row=10, column=0, columnspan=3)
+        self.generateButton.grid(row=18, column=0, columnspan=3)
 
         self.playButton = Button(
             self.root,
@@ -198,10 +240,10 @@ class App:
             fg="white",
             font=("Comfortaa", 15),
         )
-        self.playButton.grid(row=10, column=3, columnspan=3)
+        self.playButton.grid(row=18, column=3, columnspan=3)
 
         self.blank = Label(self.root, bg="#d2d2c9")
-        self.blank.grid(row=11, columnspan=6)
+        self.blank.grid(row=19, columnspan=6)
 
         self.root.mainloop()
 
@@ -269,18 +311,19 @@ class App:
             self.error.set("Video not found.")
 
     def chooseImageDir(self):
-        self.imageDir = filedialog.askdirectory()
+        self.imageDir.set(filedialog.askdirectory())
+
 
     def chooseAudio(self):
-        self.audio = filedialog.askopenfile(
+        self.audio.set(filedialog.askopenfile(
             mode="r", filetypes=[("Audio Files", "*.wav")]
-        ).name
+        ).name)
 
     def chooseOutDir(self):
-        self.outputDir = filedialog.asksaveasfile(
+        self.outputDir.set(filedialog.asksaveasfile(
             filetypes=[("Video Files", ".mp4"), ("Video Files", ".avi"), ("Video Files", ".flv"), (
                 "Video Files", ".m4a"), ("Video Files", ".mov"), ("Video Files", ".ogg"), ("Video Files", ".webm")],
-        ).name
+        ).name)
         os.remove(self.outputDir)
 
 App()
